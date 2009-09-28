5f3487dbfadd2353d6d0a8dce494825a0f7bd68a
Heath Anderson <handerson@knetwit.com>
2009-09-28 15:56:53 -0400

    fix iv length issue

--- lib/string_encryption/encryption.rb
+++ lib/string_encryption/encryption.rb
@@ -7,7 +7,7 @@ module Knet
       encrypted_data = URI.unescape(encrypted_data)
       encrypted_data = Base64.decode64(encrypted_data)
       
-      des.iv =  encrypted_data.slice!(0,16)
+      des.iv =  encrypted_data.slice!(0,8)
       
       des.update(encrypted_data) + des.final  
     end
@@ -17,7 +17,7 @@ module Knet
       des.encrypt
       des.key = ENV['STRING_ENCRYPTION_KEY']
       
-      des.iv = iv = ActiveSupport::SecureRandom.hex(8)
+      des.iv = iv = ActiveSupport::SecureRandom.hex(4)
       
       data = des.update(string) + des.final  
       data = iv + data
